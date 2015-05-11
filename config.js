f44b84bfa196d0d7db11f234b2bc86abd80e7d8d
Moufid Larbi <moufid4@gmail.com>
2015-05-11 23:15:19 +0100

    adding mongolab

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	"database": "",
+	"database": "mongodb://<dbuser>:<dbpassword>@ds035907.mongolab.com:35907/userstory",
 	"port": process.env.PORT || 3000,
 	"secretKey": "moufid"
 }
