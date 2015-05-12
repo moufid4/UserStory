ede5aeca3371fd7ed3555c7e822415739a1bd86b
Moufid Larbi <moufid4@gmail.com>
2015-05-12 01:11:42 +0100

    creating a login api

--- app/routes/api.js
+++ app/routes/api.js
@@ -3,6 +3,19 @@ var config = require('../../config');
 
 var secretKey = config.secretKey;
 
+var jsonwebtoken = require('jsonwebtoken');
+
+function createToken(user) {
+	var token = jsonwebtoken.sign({
+		_id: user._id,
+		name: user.name,
+		username: user.username
+	}, secretKey, {
+		expirtesInMinute: 1440
+	})
+	return token;
+}
+
 module.exports = function(app, express) {
 	var api = express.Router();
 
@@ -37,6 +50,29 @@ module.exports = function(app, express) {
 	
 	});
 
+	api.post('/login', function(req, res) {
+		User.findOne({ 
+			username: req.body.username
+		}).select('password').exec(function(err, user) {
+			if(err) throw err;
+			if(!user){
+				res.send({message: "User doesnt exist"});
+			} else if(user) {
+				var validPassword = user.comparePassword(req.body.password);
+				if(!validPassword) {
+					res.send({ message: "Invalid Password"});
+				} else {
+					// success -> token
+					var token = createToken(user);
+					res.json({
+						success: true,
+						message: "Successfuly logged in",
+						token: token
+					});
+				}
+			}
+		});
+	});
 
 	return api;
 }
