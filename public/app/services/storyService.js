cfa9eef11bc522f96f3e028bf480fb8171f392b8
Moufid Larbi <moufid4@gmail.com>
2015-05-13 15:09:07 +0100

    creating socket.io factory

--- public/app/services/storyService.js
+++ public/app/services/storyService.js
@@ -13,3 +13,28 @@ angular.module('storyService', [])
 
 	return storyFactory;
 })
+
+.factory('socket.io', function($rootScope) {
+	var socket = io.connect();
+	return {
+		on: function(eventName, callback) {
+			socket.on(eventName, function() {
+				var args = arguments;
+				$rootScope.$apply(function() {
+					callback.apply(socket, args);
+				});
+			});
+		},
+
+		emit: function(eventName, data, callback) {
+			socket.emit(eventName, data, function() {
+				var args = arguments;
+				$rootScope.apply(function() {
+					if(callback) {
+						callback.aply(socket, args);
+					}
+				});
+			});
+		}
+	};
+});
