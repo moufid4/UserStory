39ba6d1d452c9806aacb33e0a17ac508744331f7
Moufid Larbi <moufid4@gmail.com>
2015-05-13 02:25:59 +0100

    changing input type to password

--- public/app/services/userService.js
+++ public/app/services/userService.js
@@ -2,6 +2,7 @@ angular.module('userService', [])
 
 .factory('User', function($http) {
 	var userFactory = {};
+	
 	userFactory.create = function(userData) {
 		return $http.post('/api/signup', userData);
 	}
