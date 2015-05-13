89785912d014ae6bec9f08ee18211221f27491a9
Moufid Larbi <moufid4@gmail.com>
2015-05-13 14:01:57 +0100

    creating a story service

---
+++ public/app/services/storyService.js
@@ -0,0 +1,15 @@
+angular.module('storyService', [])
+
+.factory('Story', function($http) {
+	var storyFactory = {}
+
+	storyFactory.createStory = function(storyData) {
+		return $http.post('/api', storyData);
+	}
+
+	storyFactory.getStory = function() {
+		return $http.get('/api');
+	}
+
+	return storyFactory;
+})
