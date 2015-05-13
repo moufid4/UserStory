4b174c58c8bde8ec7bd5053184b7568c665345e9
Moufid Larbi <moufid4@gmail.com>
2015-05-13 23:05:13 +0100

    errors fix

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -12,6 +12,10 @@ angular.module('appRoutes', ['ngRoute'])
 			.when('/login', {
 				templateUrl: 'app/views/pages/login.html'
 			})
+
+			.when('/logout', {
+				templateUrl: 'app/views/pages/home.html'
+			})
 			
 			.when('/signup', {
 				templateUrl: 'app/views/pages/signup.html'
