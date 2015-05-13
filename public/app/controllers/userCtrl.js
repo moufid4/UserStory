0a21cd099313b5be0789e6064b638128ea09b3f6
Moufid Larbi <moufid4@gmail.com>
2015-05-13 01:57:22 +0100

    adding sign-up route

--- public/app/controllers/userCtrl.js
+++ public/app/controllers/userCtrl.js
@@ -12,7 +12,9 @@ angular.module('userCtrl', ['userService'])
 .controller('UserCreateController', function(User, $location, $window) {
 	var vm = this;
 	vm.signupUser = function() {
+		
 		vm.message = '';
+		
 		User.create(vm.userData)
 			.then(function(response) {
 				vm.userData = {};
